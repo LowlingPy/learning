0404b3aae4a23f9bd7b63d5a07cbfa81fb0c3480
LowlingPy <Neo_Dylan@outlook.com>
2023-08-23 21:32:23 +0330

    First game geuss a random number

--- find_random_number.py
+++ find_random_number.py
@@ -1,5 +1,7 @@
 # Camputer chose a random number and person try to find number
 
+import random
+
 for i in range(10):
     print('!!!', end='')
 print('GUESS NUMBER FASTER', end='')
@@ -7,17 +9,43 @@ for i in range(10):
     print('!!!', end='')
 print()
 
-import random
+list_of_scores = []
 
-random_number = random.randint(0, 1000)
-
-geuss = int(input('Enter your guess : '))
-
-while random_number > geuss:
-    print(f'{geuss} is lower than answer, guess higher')
-    geuss = int(input('Try again : '))
-    while random_number < geuss:
-        print(f'{geuss} is higher than answer, guess lower')
-        geuss = int(input('Try again : '))
-if random_number == geuss:
-    print(f'we have a winner the answer is {geuss} ')
+while True:
+    print('''
+    1. Start a game
+    2. Show my score
+    3. Save and Exit
+    ''')
+    choice = input('Enter your short key :')
+    if choice == '1':
+        count = 0
+        random_number = random.randint(0, 1000)
+        print(random_number)
+        while True:
+            geuss = int(input('Enter your guess : '))
+            count = count + 1
+            if random_number > geuss:
+                print(f'{geuss} is lower than answer, guess higher')
+            elif random_number < geuss:
+                print(f'{geuss} is higher than answer, guess lower')
+            else:
+                print(f'we have a winner the answer is {geuss} ')
+                list_of_scores.append(count)
+                break
+    if choice == '2':
+        # print(list_of_scores)
+        # data_score = open('data_score.txt', 'r')
+        # list_of_scores.append(data_score)
+        # data_score.close()
+        sum = 0
+        for i in list_of_scores:
+            sum = sum + i
+        average = sum / len(list_of_scores)
+        print(average)
+    if choice == '3':
+        print('***GOODBYE***')
+        # data_score = open('data_score.txt', 'w')
+        # data_score.writelines(list_of_scores)
+        # data_score.close()
+        break
