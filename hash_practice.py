2e57edca77919cb028d17efebaae4902c1ea63f6
LowlingPy <neo_dylan@outlook.com>
2023-10-14 11:17:42 +0330

    saving data for hashing and failed

--- hash_practice.py
+++ hash_practice.py
@@ -1,18 +1,27 @@
-# just practice hashing
-import bcrypt
+# # just practice hashing
+# import bcrypt
+#
+# # Declaring our password
+# password = b'123'
+#
+# # Adding the salt to password
+# salt = bcrypt.gensalt()
+# # Hashing the password
+# hashed = bcrypt.hashpw(password, salt)
+#
+# # printing the salt
+# print("Salt :")
+# print(salt)
+#
+# # printing the hashed
+# print("Hashed")
+# print(hashed)
 
-# Declaring our password
-password = b'123'
-
-# Adding the salt to password
-salt = bcrypt.gensalt()
-# Hashing the password
-hashed = bcrypt.hashpw(password, salt)
-
-# printing the salt
-print("Salt :")
-print(salt)
-
-# printing the hashed
-print("Hashed")
-print(hashed)
+import json
+password = 'akbar'
+hash_pw = hash(password)
+print(hash_pw)
+data_opened = open('testig_hash.txt', 'w+')
+save = json.dumps(hash_pw)
+data = data_opened.write(save)
+data_opened.close()
