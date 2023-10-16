eafc6be648561bdf7e497eea5e15e2c0e1baf3de
LowlingPy <neo_dylan@outlook.com>
2023-10-16 11:50:53 +0330

    "bcrypt library" used for hashing with salt was successful on test stage.

--- hash_practice.py
+++ hash_practice.py
@@ -1,20 +1,36 @@
 # # just practice hashing
-# import bcrypt
-#
-# password = b'123'
-#
-# # Adding the salt to password
-# salt = bcrypt.gensalt()
-# # Hashing the password
-# hashed = bcrypt.hashpw(password, salt)
-#
-# # printing the salt
-# print("Salt :")
-# print(salt)
-#
-# # printing the hashed
-# print("Hashed")
-# print(hashed)
+import bcrypt
+
+password = b'123'
+
+# Adding the salt to password
+salt = bcrypt.gensalt()
+x= salt
+# Hashing the password
+hashed = bcrypt.hashpw(password, x)
+
+# printing the salt
+print("Salt :")
+print(salt)
+
+# printing the hashed
+print("Hashed")
+print(hashed)
+
+pw = input('enter pass: ')
+bpw = pw.encode()
+cp = bcrypt.checkpw(bpw, hashed)
+print(cp)
+
+
+
+
+
+#print("ramz :")
+#x = input()
+#z = bcrypt.hashpw(x,salt)
+#if z==hashed :print("okaye")
+
 # # Declaring our password
 
 # import json
@@ -26,7 +42,8 @@
 # data = data_opened.write(save)
 # data_opened.close()
 
-import hashlib
-password = b'123'
-m = hashlib.sha256()
-m.update(password)
+
+# import hashlib
+# password = b'123'
+# m = hashlib.sha256()
+# m.update(password)
