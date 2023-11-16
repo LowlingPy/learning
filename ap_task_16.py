041642c3b9d7a2b9260c455047a668fb651fded4
LowlingPy <neo_dylan@outlook.com>
2023-11-16 22:15:17 +0330

    graph daraye vazn

--- ap_task_16.py
+++ ap_task_16.py
@@ -1,4 +1,5 @@
 # Mahdi Mohammadi kha 982011056
+# in graph vazn dare ghablesh bedone dar nazar gereftane vazn ferestadam ino badesh ezafe kardam
 
 class Graph:
     def __init__(self):
@@ -12,7 +13,10 @@ class Graph:
 
     def get_nodes(self):
         for node in self.nodes:
-            print(str(node.value) + ' ---> ' + str([i.value for i in node.neighbors]))
+            node_neighbors = []
+            for i in node.neighbors:
+                node_neighbors.append(str(i['node'].value) + ':' + str(i['weight']))
+            print(str(node.value) + ' ---> ' + str(node_neighbors))
 
 
 class Node:
@@ -20,11 +24,12 @@ class Node:
         self.value = value
         self.neighbors = []
 
-    def add_neighbor(self, node):
-        self.neighbors.append(node)
-        node.neighbors.append(self)
-    def delete_node(self, node):
-        self.neighbors.remove(node)
+    def add_neighbor(self, node, weight=1):
+        self.neighbors.append(dict(node=node, weight=weight))
+        node.neighbors.append(dict(node=self, weight=weight))
+
+    def delete_node(self, node, weight):
+        self.neighbors.remove(dict(node=node, weight=weight))
 
 
 graph = Graph()
@@ -32,8 +37,8 @@ node1 = Node(1)
 node2 = Node(2)
 node3 = Node(3)
 
-node1.add_neighbor(node2)
-node2.add_neighbor(node3)
+node1.add_neighbor(node2, 4)
+node2.add_neighbor(node3, 3)
 graph.add_node(node1)
 graph.add_node(node2)
 graph.add_node(node3)
