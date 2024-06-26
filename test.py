09e742a114390de9408d561204eb6ee844e61c39
LowlingPy <Neo_Dylan@outlook.com>
2024-06-26 16:38:47 +0330

    some code for using math and hamster

--- test.py
+++ test.py
@@ -250,35 +250,51 @@
 #     name: str
 #     subjects: List[str] = []
 
+#
+# from typing import List, Optional
+# from sqlalchemy import ForeignKey, String
+# from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+#
+#
+# class Base(DeclarativeBase):
+#     pass
+#
+#
+# class User(Base):
+#     __tablename__ = "user_account"
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     name: Mapped[str] = mapped_column(String(30))
+#     fullname: Mapped[Optional[str]]
+#     addresses: Mapped[List["Address"]] = relationship(
+#         back_populates="user", cascade="all, delete-orphan"
+#     )
+#
+#     def __repr__(self) -> str:
+#         return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
+#
+#
+# class Address(Base):
+#     __tablename__ = "address"
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     email_address: Mapped[str]
+#     user_id: Mapped[int] = mapped_column(ForeignKey("user_account.id"))
+#     user: Mapped["User"] = relationship(back_populates="addresses")
+#
+#     def __repr__(self) -> str:
+#         return f"Address(id={self.id!r}, email_address={self.email_address!r})"
 
-from typing import List, Optional
-from sqlalchemy import ForeignKey, String
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
+# T it's mean total coin and P its your pph
 
-class Base(DeclarativeBase):
-    pass
+t = int(input('Enter your Total coins: '))
+p = int(input('Enter your PPH: '))
 
-
-class User(Base):
-    __tablename__ = "user_account"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(30))
-    fullname: Mapped[Optional[str]]
-    addresses: Mapped[List["Address"]] = relationship(
-        back_populates="user", cascade="all, delete-orphan"
-    )
-
-    def __repr__(self) -> str:
-        return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
-
-
-class Address(Base):
-    __tablename__ = "address"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email_address: Mapped[str]
-    user_id: Mapped[int] = mapped_column(ForeignKey("user_account.id"))
-    user: Mapped["User"] = relationship(back_populates="addresses")
-
-    def __repr__(self) -> str:
-        return f"Address(id={self.id!r}, email_address={self.email_address!r})"
+while True:
+    a = int(input('Enter card price:'))
+    b = int(input('Enter card profit:'))
+    now = t * p
+    maybe = (t - a) * (p + b)
+    if now < maybe:
+        print('Buy')
+    elif now > maybe:
+        print('Do Not recommended')
